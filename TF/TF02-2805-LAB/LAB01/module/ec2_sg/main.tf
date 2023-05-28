f81343de63e7d59c80c2f7cc6defaa312b56343a
Carter Truong Do <dotruong128@gmail.com>
2023-05-28 16:33:46 -0700

    commit-tf02-lab-truongdo

---
+++ TF/TF02-2805-LAB/LAB01/module/ec2_sg/main.tf
@@ -0,0 +1,47 @@
+
+locals {
+  ami           = var.ami
+  instance_type = var.instance_type
+  name          = var.project_name
+
+  vpc_id    = var.vpc_id
+  subnet_id = var.subnet_id
+}
+
+resource "aws_instance" "myapp" {
+  ami           = local.ami
+  instance_type = local.instance_type
+
+  tags = {
+    Name = local.name
+  }
+
+  security_groups = [aws_security_group.allow_ssh.name]
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh-lab2"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = local.vpc_id
+
+  ingress { // inbound
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress { // outbound
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
